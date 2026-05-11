b1312898067cb2a2a2a58c241cba7fe2ae40ee4c
Ayawo II <kossiabel99@gmail.com>
2026-05-11 14:51:17 +0000

    modification des API pour le backend en prod

--- learning/app/data/api.ts
+++ learning/app/data/api.ts
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api"
+const API_URL = "https://tdoo-backend.onrender.com/api"
 
 export async function getTasks(token: string) {
   const res = await fetch(`${API_URL}/tasks`, {
--- learning/app/register/page.tsx
+++ learning/app/register/page.tsx
@@ -10,7 +10,7 @@ export default function RegisterPage() {
   const router = useRouter()
 
   async function handleSubmit() {
-    const res = await fetch("http://localhost:5000/api/auth/register", {
+    const res = await fetch("https://tdoo-backend.onrender.com/api/auth/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, password }),
